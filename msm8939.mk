6d8c8f91eb50d98a037fe329743dc4e603235a45
Danny Baumann <dannybaumann@web.de>
2015-09-04 14:46:43 +0200

    Fix copy'n'paste mistake.
    Change-Id: I1fed50a89151a73b1849e237071bbb0cce1dc73b

--- msm8939.mk
+++ msm8939.mk
@@ -143,9 +143,9 @@ PRODUCT_PACKAGES += \
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
-    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
-    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
+    $(LOCAL_PATH)/configs/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/qmi_config.xml:system/etc/data/qmi_config.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
