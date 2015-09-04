8b89235c4705e5e16ccaa0763c8d6a6272efc573
Danny Baumann <dannybaumann@web.de>
2015-09-04 13:38:34 +0200

    Add netmgr and qmi configs.
    Change-Id: I41509c13fc4cc8e5a0b26f82ff13864153bd2603

--- msm8939.mk
+++ msm8939.mk
@@ -141,6 +141,12 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     power.msm8916
 
+# Qualcomm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
