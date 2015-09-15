03bc15a0d1a9814dab91423cc13b86687c92a519
Naresh Tanniru <ntanniru@codeaurora.org>
2015-09-15 19:36:00 +0200

    audio: add audio related property overrides
    override audio_hal.period_size and audio_hal.period_size
    properties to optimize the playback latency.
    
    Change-Id: I5bbe82af6be4da363985455a80004200a425faac

--- system_prop.mk
+++ system_prop.mk
@@ -34,6 +34,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false
 
+# Reduce client buffer size for fast audio output tracks
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1
+
+# Low latency audio buffer size in frames
+PRODUCT_PROPERTY_OVERRIDES += \
+    audio_hal.period_size=192
+
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.call_ring.multiple=false \
