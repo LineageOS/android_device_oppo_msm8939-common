a42c92f4f5e13d792a081d04194a14c65faf16d1
Danny Baumann <dannybaumann@web.de>
2015-10-05 14:53:49 +0200

    Install bluetooth configuration script.
    Fixes bluetooth address loading.
    
    Change-Id: I5c2e54c56d42b24dec28c16716d26eb10da1d3ca

--- msm8939.mk
+++ msm8939.mk
@@ -73,6 +73,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    init.qcom.bt.sh
+
 # Thermal-engine
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
