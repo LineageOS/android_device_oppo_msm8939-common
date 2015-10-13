271a3ab6e82e28f09fe01dc3a13cd39c573ec1c5
Danny Baumann <dannybaumann@web.de>
2015-10-13 08:34:40 +0200

    Move Wifi NV image to device repos.
    Change-Id: I919c4e0c30af5fd31619f7e9b23cc5d5506b9b2e

--- msm8939.mk
+++ msm8939.mk
@@ -193,8 +193,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 # IRQ balance
 PRODUCT_COPY_FILES += \
