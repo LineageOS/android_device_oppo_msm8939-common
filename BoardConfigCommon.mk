a3ea28002effba0dddb682a0bd6102ae373ba7e0
Michael Bestas <mikeioannina@gmail.com>
2015-10-29 19:14:08 +0200

    msm8939: Stop using custom mkbootimg.mk
    * Build system already has support for dt.img (BOARD_KERNEL_SEPARATED_DT)
    
    Change-Id: I519b9c2c88570af61c6c4451a81bf7d67854152b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,12 +56,12 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(COMMON_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_MKBOOTIMG_ARGS := --tags_offset 0x00000100
+TARGET_CUSTOM_DTBTOOL := dtbToolOppo
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8939
 ifneq ($(FORCE_32_BIT),true)
 TARGET_KERNEL_ARCH := arm64
