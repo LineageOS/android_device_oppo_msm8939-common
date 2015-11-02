cf888044b21fb6ef00c0246b7963b872680fb896
Rashed Abdel-Tawab <rashed@linux.com>
2015-11-01 21:25:19 -0500

    msm8939: Update to OpenGLES 3.1
    Android 5.0 and forward support OpenGLES 3.1, but for some reason
    no one had implemented it yet. Might as well start now
    
    Change-Id: Ieee4697dafd9e67f399db7e9e6d20dd91c58be3a

--- msm8939.mk
+++ msm8939.mk
@@ -29,6 +29,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
