4ff314bc653f8f531a47a50ff9cc985d7bdddd67
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-03-26 01:32:04 +0200

    msm8939: Move TrustZone assertion to devices
    Change-Id: I37edd6235929104ec4da584a7ff513899006e8dd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,9 +76,6 @@ endif
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 
-# Assertions
-TARGET_BOARD_INFO_FILE ?= device/oppo/msm8939-common/board-info.txt
-
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 AUDIO_FEATURE_ENABLED_FLUENCE := true
