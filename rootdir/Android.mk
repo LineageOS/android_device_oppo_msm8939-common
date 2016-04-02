209a45762b90600cd69ffe0feaa42123cf1a791a
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-04-02 19:18:22 +0300

    msm8939: Split v1.0-v2.0/v3.0 power configuration
    * r5/r7 are msm8939 v1.0 while r7plus/f1f are msm8939 v3.0
    * QC has different power configuration for v3.0, so split the config
    
    Change-Id: I31140d18f3a80938dbe9c761a066dfd50c20bef1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,10 +28,16 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+ifneq ($(FORCE_32_BIT),true)
+POWER_SCRIPT := init.qcom.power.v3.rc
+else
+POWER_SCRIPT := init.qcom.power.v1.rc
+endif
 LOCAL_MODULE       := init.qcom.power.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power.rc
+LOCAL_SRC_FILES    := etc/$(POWER_SCRIPT)
+LOCAL_MODULE_STEM  := init.qcom.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
