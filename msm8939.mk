f42ccd4053142fe910aa31c62eab60d80ba3f003
Rashed Abdel-Tawab <rashed@linux.com>
2016-05-01 01:08:14 -0400

    msm8939: Gello time
    Change-Id: Ibf1d7d43983d616e1eff4c3bf4588da198d60cd0

--- msm8939.mk
+++ msm8939.mk
@@ -71,6 +71,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Browser
+PRODUCT_PACKAGES += \
+    Gello
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
