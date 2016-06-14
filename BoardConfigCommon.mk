238607a7ba70eba1c98fdd505c8bb5d139f81ae2
Danny Baumann <dannybaumann@web.de>
2016-06-14 12:57:26 +0200

    Disable cluster plug, enable core_ctl.
    Change-Id: Iec56766f518bd1cd2fa59e1a007676df6a59477e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,7 +132,6 @@ TARGET_PROVIDES_LIBLIGHT := true
 MALLOC_IMPL := dlmalloc
 
 # Power
-TARGET_POWERHAL_SET_INTERACTIVE_EXT := $(COMMON_PATH)/power/power_ext.c
 TARGET_POWERHAL_VARIANT := qcom
 
 # Qualcomm support
