042a503c818dce028d856615426017f55fd3952d
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-06-17 17:46:37 +0300

    msm8939: Add GPS config files
    * From spectrum marshmallow release
    
    Change-Id: I81f1ca3c838b6906bf557a6f21ed82851de1c8ca

--- msm8939.mk
+++ msm8939.mk
@@ -107,7 +107,10 @@ PRODUCT_PACKAGES += \
     gps.msm8916
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # IRQ balance
 PRODUCT_COPY_FILES += \
