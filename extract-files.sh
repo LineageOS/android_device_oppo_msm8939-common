01e7ee0e79cc17adf3e644397b62acac1f22ff49
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-08-02 00:57:47 +0300

    msm8939: Add common device-specific proprietary blob lists
    * Reduce duplication in device trees
    * Make it easier to commonize some of these blobs
    * Also use the new generic extract utils in vendor/cm
    
    Change-Id: I7c19dda0731dd8c28f4698a4cdc275a8d40156bf

--- extract-files.sh
+++ extract-files.sh
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +17,7 @@
 
 set -e
 
-# Load extract_utils and do some sanity checks
+# Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
@@ -35,27 +35,31 @@ if [ -z "$BITS" ]; then
     exit 1
 fi
 
+if [ $# -eq 0 ]; then
+    SRC=adb
+else
+    if [ $# -eq 1 ]; then
+        SRC=$1
+    else
+        echo "$0: bad number of arguments"
+        echo ""
+        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+        echo ""
+        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+        echo "the device using adb pull."
+        exit 1
+    fi
+fi
+
 # Initialize the helper for common device
 setup_vendor "$DEVICE_COMMON-$BITS" "$VENDOR" "$CM_ROOT" true
 
-# Copyright headers and common guards
-if [ "$BITS" == "32" ]; then
-    write_headers "r5 r7"
-else
-    write_headers "f1f r7plus r7sf"
-fi
-
-write_makefiles "$MY_DIR"/proprietary-files-$BITS.txt
-
-write_footers
+extract "$MY_DIR"/proprietary-files-$BITS.txt "$SRC"
 
 # Reinitialize the helper for device
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-# Copyright headers and guards
-write_headers
+extract "$MY_DIR"/device-proprietary-files-$BITS.txt "$SRC"
+extract "$MY_DIR"/../$DEVICE/device-proprietary-files.txt "$SRC"
 
-write_makefiles "$MY_DIR"/device-proprietary-files-$BITS.txt
-write_makefiles "$MY_DIR"/../$DEVICE/device-proprietary-files.txt
-
-write_footers
+"$MY_DIR"/setup-makefiles.sh
