1317cc94d21301052fb561ad38dd97159abfb461
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-10-04 22:19:05 +0300

    msm8939: Cleanup init.qcom.bt.sh
    * Remove bluez support
    * Set properties in system_prop.mk
    * Bring closer to AOSP devices' script
    
    Change-Id: I6eeb69487b02070a84bc2958fb8a939a684ab915

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,22 @@
 # System Properties for Oppo MSM8939
 #
 
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bluetooth.hfp.ver=1.7 \
+    ro.qualcomm.bt.hci_transport=smd
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bluetooth.dun=true \
+    ro.bluetooth.sap=true \
+    ro.qualcomm.bluetooth.ftp=true \
+    ro.qualcomm.bluetooth.hfp=true \
+    ro.qualcomm.bluetooth.hsp=true \
+    ro.qualcomm.bluetooth.map=true \
+    ro.qualcomm.bluetooth.nap=true \
+    ro.qualcomm.bluetooth.opp=true \
+    ro.qualcomm.bluetooth.pbap=true
+
 # Display
 #
 # OpenGLES:
