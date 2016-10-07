4bc8f855ddc38b95d01a72c980f3ee2c11ae056a
Paras Nagda <pnagda@codeaurora.org>
2016-10-07 19:00:39 +0530

    msm8939: mediacodec: Allow select and eventfd syscalls for 8916
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    
    Change-Id: Iff6e2f7fc0562a520f56cbf4bbce72f1af545f9c

--- msm8939.mk
+++ msm8939.mk
@@ -195,6 +195,11 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     calmodule.cfg \
