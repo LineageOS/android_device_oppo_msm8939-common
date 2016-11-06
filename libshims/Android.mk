2e4c411dd9687b6167ce358975b266c844ffb385
Arne Coucheron <arco68@gmail.com>
2016-11-06 19:23:32 +0200

    msm8939: Add shim for libwvm.so
    Change-Id: I1ac58a108783ae1aabe3f3f061b2444925f684db

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -10,7 +10,15 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := gui
 LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid libui
 LOCAL_MODULE := libshim_camera
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := wvm_shim.cpp
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshim_wvm
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
