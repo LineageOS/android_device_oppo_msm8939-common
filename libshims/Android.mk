48f108bfe31345bb0ba8526cb2082e6e04c9d955
Kilian von Pflugk <kilian@nood.tv>
2016-11-12 22:19:37 +0100

    Revert "msm8939: Add shim for libwvm.so"
    This reverts commit 377ccd4d1a49ecbe6087f9ebb0d205ad8955cbb7.
    
    Change-Id: I129d2e02c061530d0df82f52cb604f8f53f66b68

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -10,16 +10,7 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := gui
 LOCAL_SHARED_LIBRARIES := libgui libutils liblog libbinder libandroid libui
 LOCAL_MODULE := libshim_camera
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := wvm_shim.cpp
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
-LOCAL_MODULE := libshim_wvm
-LOCAL_MODULE_TAGS := optional
-LOCAL_32_BIT_ONLY := true
-
-include $(BUILD_SHARED_LIBRARY)
