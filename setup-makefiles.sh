2c300207befe10f3634a6656850304ccef339cc7
Michael Bestas <mikeioannina@gmail.com>
2016-12-15 10:32:00 -0800

    msm8939: Update for graphics makefile changes
    Change-Id: I56921774bd15ad3e8147620dc58c320fdf9d0efc

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -47,7 +47,12 @@ fi
 
 write_makefiles "$MY_DIR"/proprietary-files-$BITS.txt
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8916/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+if [ "$BITS" == "32" ]; then
+    printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8916-32/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+else
+    printf '\n%s' "\$(call inherit-product, vendor/qcom/binaries/msm8916-32/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+    printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8916-64/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+fi
 
 write_footers
 
