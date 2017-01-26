85a7dd5d712d5b659b1ac9d9df254c9d3b4579ad
Kilian von Pflugk <kilian@nood.tv>
2017-01-26 22:25:24 +0100

    msm8939-common: Add core_ctl props
    * perfd uses them to restore proper min/max cpus values
    
    Change-Id: I39e910afc48c869f7796da9c1ed78996798cf427

--- system_prop.mk
+++ system_prop.mk
@@ -74,7 +74,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # QC vendor extension
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.core_ctl_min_cpu=0 \
+    ro.core_ctl_max_cpu=4
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
