a4f333b7fdafb37f7ec83784f79aa409fc8ff74b
Jeff Sharkey <jsharkey@android.com>
2017-03-25 22:57:15 -0600

    msm8939-common: Enable partitions on loop devices.
    This is used by the new "virtual disk" feature used by vold to help
    debug and test storage code on devices without physical media.
    
    Bug: 34903607
    Test: builds, boots, virtual disk works
    Change-Id: Icd43ac6f9b34241419aab17285c0afa77f0916ec

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,7 +53,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8939
