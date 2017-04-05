c8a4cbb680afeb731bce8a49f19c7021d5b2fb48
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    msm8939: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    (cherry picked from commit db1b6058200fc5255f519cf6609fca7619d6743e)

--- msm8939.mk
+++ msm8939.mk
@@ -17,6 +17,8 @@
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := framework-res
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
