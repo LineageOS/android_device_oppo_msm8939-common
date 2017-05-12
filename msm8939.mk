c9b863ed55381ee231a019bcf1f53fe5fbbb193d
Paul Keith <javelinanddart@gmail.com>
2017-05-12 12:22:35 -0700

    msm8939: Kill Gello
    Change-Id: Id221ec64c9fb2d2d3aef72fefdae390077dd11d8
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- msm8939.mk
+++ msm8939.mk
@@ -78,10 +78,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
-# Browser
-PRODUCT_PACKAGES += \
-    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
