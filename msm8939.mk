5273592103d96d9a8baf02bd6e5bef093620f95e
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:18:12 +0200

    msm8939: Build GNSS HAL
    Change-Id: I83aec8b57f18383642d83110a8720fdff1b92546

--- msm8939.mk
+++ msm8939.mk
@@ -107,6 +107,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8916
 
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
