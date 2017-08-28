f9ded8b77d3c47ec60fd10ae96e92598b301e0eb
LuK1337 <priv.luk@gmail.com>
2017-08-28 11:59:53 +0200

    msm8939: gps: Add flp shim
    Change-Id: I4cf64915561c60b24a1ab85f65f73b012385ea56

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,7 +149,9 @@ BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 # Shim
 TARGET_LD_SHIM_LIBS := \
+    /system/vendor/lib64/libflp.so|libshims_flp.so \
     /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
+    /system/vendor/lib/libflp.so|libshims_flp.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
 
 # Touchscreen
