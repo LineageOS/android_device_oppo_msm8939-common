7341c83aff37660bf8fa067663b9dabbda014666
LuK1337 <priv.luk@gmail.com>
2017-08-28 11:59:53 +0200

    msm8939: gps: Add flp shim
    Change-Id: I4cf64915561c60b24a1ab85f65f73b012385ea56

--- msm8939.mk
+++ msm8939.mk
@@ -119,7 +119,8 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8916 \
-    libshims_get_process_name
+    libshims_get_process_name \
+    libshims_flp
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
