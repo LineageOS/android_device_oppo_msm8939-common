88fcf48d27484764d6fb45b3d02d9f9f895e8381
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-30 17:00:16 +0800

    msm8939: Change WCNSS_qcom_cfg.ini file path
    Change-Id: I48381daba9c839379302e638cc8a3d6388563f26

--- Android.mk
+++ Android.mk
@@ -123,7 +123,7 @@ $(WV_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(WV_SYMLINKS)
 
-WCNSS_CONFIG_SYMLINK := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+WCNSS_CONFIG_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 $(WCNSS_CONFIG_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@echo "WCNSS config link: $@"
 	@mkdir -p $(dir $@)
