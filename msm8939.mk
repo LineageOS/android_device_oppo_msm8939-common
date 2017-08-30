45e6059948a39424fa5ad276a9bd2756ad9d29d2
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-30 17:00:16 +0800

    msm8939: Change WCNSS_qcom_cfg.ini file path
    Change-Id: I48381daba9c839379302e638cc8a3d6388563f26

--- msm8939.mk
+++ msm8939.mk
@@ -230,7 +230,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Call the proprietary setup
 ifeq ($(FORCE_32_BIT),true)
