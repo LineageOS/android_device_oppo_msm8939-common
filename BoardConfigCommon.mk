70ed2011c1b48d552fc69e5a968df0c188e18372
Michael Bestas <mkbestas@lineageos.org>
2017-10-06 20:45:04 +0300

    msm8939: Update kernel flags
    * Update for 64bit devices Spectrum bootloader compatibility
    
    Change-Id: I89c45af0340050b70660e6882efe4468f7ab1992

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,13 +61,14 @@ ENABLE_CPUSETS := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --tags_offset 0x00000100
-TARGET_CUSTOM_DTBTOOL := dtbToolOppo
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8939
 ifneq ($(FORCE_32_BIT),true)
-TARGET_KERNEL_ARCH := arm64
-TARGET_USES_UNCOMPRESSED_KERNEL := true
+BOARD_KERNEL_IMAGE_NAME := Image-dtb
+else
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_CUSTOM_DTBTOOL := dtbToolOppo
 endif
 
 # Include path
