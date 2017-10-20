c812b54e6ffb7cb6cea2a9769de58385db061a74
jrior001 <jriordan001@gmail.com>
2017-10-19 23:00:45 -0400

    msm8939: convert CameraParameters header to shim lib
    Change-Id: I31c7c3346c8fac4020cd86822c9cfb1a6db26b77

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,6 +153,7 @@ BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib64/libflp.so|libshims_flp.so \
     /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
+    /system/vendor/lib/hw/camera.vendor.msm8916.so|libshims_camera_parameters.so \
     /system/vendor/lib/libflp.so|libshims_flp.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
 
