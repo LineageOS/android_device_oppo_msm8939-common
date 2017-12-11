24a9edb30999a4ef67597934946669b097108669
Shaikh Shadul <sshadu@codeaurora.org>
2017-12-11 19:33:42 +0530

    msm8939: Sensors: Wait for reliable accel samples
    Drop improper accel samples with all zeros until the first
    proper sample is received.
    
    Change-Id: Ic949f31120ae12acda0f36b430593af3175d0cd1

--- sensors/Accelerometer.cpp
+++ sensors/Accelerometer.cpp
@@ -52,6 +52,7 @@ AccelSensor::AccelSensor()
 	: SensorBase(NULL, "accelerometer"),
 	  mInputReader(4),
 	  mHasPendingEvent(false),
+	  mAbsEventReceived(false),
 	  mEnabledTime(0)
 {
 	mPendingEvent.version = sizeof(sensors_event_t);
@@ -73,6 +74,7 @@ AccelSensor::AccelSensor(char *name)
 	: SensorBase(NULL, "accelerometer"),
 	  mInputReader(4),
 	  mHasPendingEvent(false),
+	  mAbsEventReceived(false),
 	  mEnabledTime(0)
 {
 	mPendingEvent.version = sizeof(sensors_event_t);
@@ -95,6 +97,7 @@ AccelSensor::AccelSensor(SensorContext *context)
 	: SensorBase(NULL, NULL, context),
 	  mInputReader(4),
 	  mHasPendingEvent(false),
+	  mAbsEventReceived(false),
 	  mEnabledTime(0)
 {
 	mPendingEvent.version = sizeof(sensors_event_t);
@@ -215,6 +218,7 @@ again:
 		int type = event->type;
 		if (type == EV_ABS) {
 			float value = event->value;
+			mAbsEventReceived = true;
 			if (event->code == EVENT_TYPE_ACCEL_X) {
 				mPendingEvent.data[0] = value * CONVERT_ACCEL_X;
 			} else if (event->code == EVENT_TYPE_ACCEL_Y) {
@@ -242,7 +246,7 @@ again:
 							mPendingEvent.timestamp = timevalToNano(event->time);
 						}
 						mPendingEvent.timestamp -= sysclk_sync_offset;
-						if (mEnabled) {
+						if (mEnabled && mAbsEventReceived) {
 							*data++ = mPendingEvent;
 							numEventReceived++;
 							count--;
