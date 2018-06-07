4026e2dc63e820d30e952777f59c1f4d10fbb051
Danny Baumann <dannybaumann@web.de>
2018-06-07 11:44:44 +0200

    msm8939: wcnss_oppo: Use wlan-caf includes.
    Change-Id: Ib8260b50cca519b8a75e0c0b54a34bae9e7c223b

--- wcnss_oppo/Android.mk
+++ wcnss_oppo/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := wcnss_oppo_client.c
 
-LOCAL_C_INCLUDES += hardware/qcom/wlan/wcnss_service
+LOCAL_C_INCLUDES += hardware/qcom/wlan-caf/wcnss_service
 LOCAL_CFLAGS += -Wall
 
 LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
