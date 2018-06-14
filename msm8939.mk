6f0a5694bc2208db9fd3a15ea45e93422264f9c8
Michael Bestas <mkbestas@lineageos.org>
2018-06-14 17:37:12 +0300

    msm8939: Move all blobs to vendor
    * Requires following hex edit to load configs from vendor:
    sed -i "s|/system/etc/data/dsi_config.xml|/vendor/etc/data/dsi_config.xml|g" vendor/lib64/libdsi_netctrl.so
    sed -i "s|/system/etc/data/dsi_config.xml|/vendor/etc/data/dsi_config.xml|g" vendor/lib/libdsi_netctrl.so
    sed -i "s|/system/etc/data/netmgr_config.xml|/vendor/etc/data/netmgr_config.xml|g" vendor/bin/netmgrd
    sed -i "s|/system/etc/data/qmi_config.xml|/vendor/etc/data/qmi_config.xml|g" vendor/bin/qmuxd
    sed -i "s|/system/etc/|/vendor/etc/|g" vendor/bin/thermal-engine
    * Also requires edit of qcnvitems.xml and qcrilhook.xml
      to load jars from vendor
    
    Change-Id: Iacb6ed803c5457f35926ea7e51cf3c2b0c6c1644

--- msm8939.mk
+++ msm8939.mk
@@ -80,11 +80,11 @@ PRODUCT_PACKAGES += \
 
 # Thermal-engine
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
 
 # MSM IPC Router configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:system/vendor/etc/sec_config
 
 # Camera
 PRODUCT_PACKAGES += \
