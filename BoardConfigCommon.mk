f7a388afcb20c6bf1046ef9a8e29eae5da4616b8
Michael Bestas <mkbestas@lineageos.org>
2018-06-17 22:00:12 +0300

    msm8939: Enable 64bit binder on 32bit devices
    Change-Id: I6444651078e7cabc000d8efa48ec2eaa3d68e922

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,6 +43,8 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
 endif
 
 # Bootloader
