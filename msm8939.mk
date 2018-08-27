0f80674fc5c3bbca94b2b7c01cd6ebf0bb457c4d
Bruno Martins <bgcngm@gmail.com>
2018-08-27 09:15:40 +0200

    msm8939-common: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- msm8939.mk
+++ msm8939.mk
@@ -214,6 +214,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
