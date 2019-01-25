e97eac6b130de726bfe001e8a4f9ec571f5c6919
Danny Baumann <dannybaumann@web.de>
2019-01-25 13:39:28 +0100

    msm8939: Convert lights HAL to HIDL
    Change-Id: I5dc2dd55560ac7b0af4d4663cba1cef041847ac5

--- msm8939.mk
+++ msm8939.mk
@@ -157,11 +157,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.msm8916
-
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.oppo_msm8939
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
