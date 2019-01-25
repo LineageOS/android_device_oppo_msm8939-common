3d508a68bb1739956779fcffe1f2f0c1840d8364
Danny Baumann <dannybaumann@web.de>
2019-01-25 13:17:30 +0100

    msm8939-common: Migrate lineagehw touch gestures to HIDL interface.
    Change-Id: I650e37fa91119413c3c4d52494b6b983b331e091

--- msm8939.mk
+++ msm8939.mk
@@ -221,6 +221,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.oppo_msm8939
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
