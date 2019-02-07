45a8271a1c51add6492176e13a41c5575a2fdeff
Paul Keith <javelinanddart@gmail.com>
2019-02-07 13:12:30 -0600

    msm8939-common: Build livedisplay sysfs service
    * Add supported interfaces to manifest while we're at it
    * Keep ISunlightEnhancement out of the manifest since
      not all devices support it
    
    Change-Id: Iaad13fcdfb603d144599133339f9c0c6a0d1c66a

--- msm8939.mk
+++ msm8939.mk
@@ -161,7 +161,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-legacymm
+    vendor.lineage.livedisplay@2.0-service-legacymm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Media configuration
 PRODUCT_COPY_FILES += \
